00cd0410bc9f2acc3b8801e72380563a1b23326e
Nikias Bassen <nikias@gmx.li>
2011-09-10 17:59:56 +0200

    Adapted for win32 (mingw) building

--- config.h
+++ config.h
@@ -46,7 +46,7 @@
 #define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the `strerror_r' function. */
-#define HAVE_STRERROR_R 1
+/* #undef HAVE_STRERROR_R */
 
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
